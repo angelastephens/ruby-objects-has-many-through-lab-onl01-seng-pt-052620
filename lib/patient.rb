0b4900a16e066e1a9dfd2dc48f5a0d80514856b2
Angela Stephens <angelastephens317@gmail.com>
2020-06-27 17:22:32 +0000

    Automatically backed up by Learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -21,7 +21,7 @@ class Patient
     self.appointments.collect {|appointment| appointment.doctor}
   end
 
-  def new_appointment(doctor, date)
+  def new_appointment(date,doctor)
     Appointment.new(date,self, doctor)
   end
   
